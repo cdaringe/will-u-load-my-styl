aebbff1503a8ecd6447c309e4ecdbfebfb4b2b35
cdaringe <christopher.dieringer@gmail.com>
2018-09-27 10:21:37 -0700

    nextify

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-octagon/lib/styles/semantic.css'
+import { FavoriteButton } from 'react-octagon'
+import { Segment } from 'semantic-ui-react'
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Segment>
+        <FavoriteButton isFavorited />
+      </Segment>
+    )
   }
 }
-
-export default App;
+export default App
